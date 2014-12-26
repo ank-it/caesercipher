0dfc88f81bc15e73d82d1d5329f1cd6a7c2c3cfd
Josh Batchelor <josh_batch@live.com.au>
2014-12-26 17:36:53 +0800

    Started the GUI. need to clear up alignments and hook up events

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -2,7 +2,9 @@ public class CaesarCipher
 {
 	public static void main(String argv[])
 	{
-		System.out.println(Cipher.decrypt(Cipher.encrypt("Hello World", 17), 17));
+		//System.out.println(Cipher.decrypt(Cipher.encrypt("Hello World", 17), 17));
+
+		CaesarCipherGui gui = new CaesarCipherGui();
 	}
 
 }
--- CaesarCipherGui.java
+++ CaesarCipherGui.java
@@ -1,4 +1,74 @@
-public class CaesarCipherGui
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.BorderFactory;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
+
+public class CaesarCipherGui extends JFrame 
 {
-	
+	public CaesarCipherGui()
+	{
+		JPanel caeserCipherPanel = new JPanel();
+		JPanel shiftPanel = new JPanel();
+
+		JMenuBar menuBar = new JMenuBar();
+		JMenu caeserCipherMenu = new JMenu("Caeser Cipher");
+		JMenuItem clear = new JMenuItem("Clear");
+		JMenuItem quit = new JMenuItem("Quit");
+		quit.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent event)
+			{
+				System.exit(0);
+			}
+		});
+
+		caeserCipherMenu.add(clear);
+		caeserCipherMenu.add(quit);
+		menuBar.add(caeserCipherMenu);
+		this.setJMenuBar(menuBar);
+
+		JLabel messageLabel = new JLabel("Message:");
+		JLabel encryptedMessageLabel = new JLabel("Encrypted Message:");
+		JLabel shiftLabel = new JLabel("Shift:");
+
+		JButton encryptButton = new JButton("Encyrpt");
+
+		JTextField messageField = new JTextField("Hello World");
+		JTextField encryptedMessageField = new JTextField();
+		encryptedMessageField.setEditable(false);
+		JTextField shiftField = new JTextField("3");
+
+		caeserCipherPanel.setLayout(new BoxLayout(caeserCipherPanel, BoxLayout.PAGE_AXIS));
+
+		caeserCipherPanel.add(messageLabel);
+		caeserCipherPanel.add(messageField);
+		caeserCipherPanel.add(encryptedMessageLabel);
+		caeserCipherPanel.add(encryptedMessageField);
+
+		shiftPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
+
+		shiftPanel.add(shiftLabel);
+		shiftPanel.add(shiftField);
+
+		caeserCipherPanel.add(shiftPanel);
+		caeserCipherPanel.add(encryptButton);
+		caeserCipherPanel.setBorder(BorderFactory.createEmptyBorder(0,10,10,10));
+
+		add(caeserCipherPanel);
+
+		this.setSize(500, 500);
+		this.setTitle("Caeser Cipher");
+		setVisible(true);
+	}
 }
